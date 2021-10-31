bb31823aaff9bf2da3585f135af6120a8b77000f
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-10-31 00:04:11 -0400

    Initialize

---
+++ CauldronPromos/Controller/MrFixer/EnlightenedMrFixerCharacterCardController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+
+namespace CauldronPromos.MrFixer
+{
+    public class EnlightenedMrFixerCharacterCardController : HeroCharacterCardController
+    {
+        public EnlightenedMrFixerCharacterCardController(Card card, TurnTakerController turnTakerController)
+            : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator UseIncapacitatedAbility(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                   
+                    
+                    break;
+                case 1:
+                    
+                    break;
+                case 2:
+                   
+                    break;
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerator UsePower(int index = 0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Testing/Promos/EnlightenedMisterFixerTests.cs
@@ -0,0 +1,76 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CauldronPromos.MrFixer;
+
+namespace CauldronPromosTests
+{
+    [TestFixture()]
+    public class EnlightenedMrFixerTests : CauldronBaseTest
+    {
+
+        private void SetupIncap(TurnTakerController villain)
+        {
+            SetHitPoints(fixer.CharacterCard, 1);
+            DealDamage(villain, fixer, 2, DamageType.Melee);
+        }
+
+        [Test()]
+        public void TestLoadEnlightenedMrFixer()
+        {
+            SetupGameController("BaronBlade", "MrFixer/CauldronPromos.EnlightenedMrFixerCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+
+            Assert.AreEqual(6, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(fixer);
+            Assert.IsInstanceOf(typeof(EnlightenedMrFixerCharacterCardController), fixer.CharacterCardController);
+
+            Assert.AreEqual(27, fixer.CharacterCard.HitPoints);
+        }
+
+        [Test()]
+        public void TestEnlightenedMrFixerInnatePower()
+        {
+            SetupGameController("BaronBlade", "MrFixer/CauldronPromos.EnlightenedMrFixerCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+
+            //Mr Fixer deals 1 target 2 radiant damage. One player discards a card.
+        }
+
+        [Test()]
+        public void TestEnlightenedMrFixerIncap1()
+        {
+            SetupGameController("BaronBlade", "MrFixer/CauldronPromos.EnlightenedMrFixerCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            SetupIncap(baron);
+
+            //One player may play a card now.
+        }
+
+        [Test()]
+        public void TestEnlightenedMrFixerIncap2()
+        {
+            SetupGameController("BaronBlade", "MrFixer/CauldronPromos.EnlightenedMrFixerCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            SetupIncap(baron);
+
+            //Destroy a target with 2 or fewer HP.
+
+        }
+
+        [Test()]
+        public void TestEnlightenedMrFixerIncap3()
+        {
+            SetupGameController("BaronBlade", "MrFixer/CauldronPromos.EnlightenedMrFixerCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            SetupIncap(baron);
+
+            //Discard the top card of 1 deck.
+        }
+    }
+}
