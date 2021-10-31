6c2802e3c446e1210d9bc4eff6b9b15773fb8462
wrhyme29 <32969943+wrhyme29@users.noreply.github.com>
2021-10-31 01:56:26 -0400

    Test

---
+++ Testing/Promos/NitroBoostAbsoluteZeroTests.cs
@@ -0,0 +1,114 @@
+﻿using Handelabra;
+using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using Handelabra.Sentinels.UnitTest;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using CauldronPromos.AbsoluteZero;
+
+namespace CauldronPromosTests
+{
+    [TestFixture()]
+    public class NitroBoostAbsoluteZeroTests : CauldronBaseTest
+    {
+
+        private void SetupIncap(TurnTakerController villain)
+        {
+            SetHitPoints(az.CharacterCard, 1);
+            DealDamage(villain, az, 2, DamageType.Melee);
+        }
+
+        [Test()]
+        public void TestLoadNitroBoostAbsoluteZero()
+        {
+            SetupGameController("BaronBlade", "AbsoluteZero/CauldronPromos.NitroBoostAbsoluteZeroCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+
+            Assert.AreEqual(6, this.GameController.TurnTakerControllers.Count());
+
+            Assert.IsNotNull(az);
+            Assert.IsInstanceOf(typeof(NitroBoostAbsoluteZeroCharacterCardController), az.CharacterCardController);
+
+            Assert.AreEqual(28, az.CharacterCard.HitPoints);
+        }
+
+        [Test()]
+        public void TestNitroBoostAbsoluteZeroInnatePower()
+        {
+            SetupGameController("BaronBlade", "AbsoluteZero/CauldronPromos.NitroBoostAbsoluteZeroCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+
+            Card focusedApertures = PutInHand("FocusedApertures");
+
+            //Play a card. Absolute Zero deals himself 1 fire and 1 cold damage.
+            DecisionSelectCards = new Card[] { focusedApertures };
+            QuickHPStorage(baron, az, legacy, haka, ra);
+            UsePower(az);
+            QuickHPCheck(0, -3, 0, 0, 0);
+            AssertInPlayArea(az, focusedApertures);
+        }
+
+        [Test()]
+        public void TestNitroBoostAbsoluteZeroIncap1()
+        {
+            SetupGameController("BaronBlade", "AbsoluteZero/CauldronPromos.NitroBoostAbsoluteZeroCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            SetupIncap(baron);
+
+            //One player may use a power now.
+            AssertIncapLetsHeroUsePower(az, 0, legacy);
+            
+        }
+
+        [Test()]
+        public void TestNitroBoostAbsoluteZeroIncap2()
+        {
+            SetupGameController("BaronBlade", "AbsoluteZero/CauldronPromos.NitroBoostAbsoluteZeroCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            SetupIncap(baron);
+
+            //Select a hero. Increase the next damage dealt by that hero by 2.
+            DecisionSelectCards = new Card[] { haka.CharacterCard };
+
+            UseIncapacitatedAbility(az, 1);
+
+            QuickHPStorage(baron);
+            DealDamage(haka, baron, 2, DamageType.Melee);
+            QuickHPCheck(-4);
+
+            //should expire after 1 use
+            QuickHPUpdate();
+            DealDamage(haka, baron, 2, DamageType.Melee);
+            QuickHPCheck(-2);
+
+        }
+
+        [Test()]
+        public void TestNitroBoostAbsoluteZeroIncap3()
+        {
+            SetupGameController("BaronBlade", "AbsoluteZero/CauldronPromos.NitroBoostAbsoluteZeroCharacter", "Legacy", "Haka", "Ra", "Megalopolis");
+            StartGame();
+            DestroyNonCharacterVillainCards();
+            SetupIncap(baron);
+
+            Card battalion = PlayCard("BladeBattalion");
+            Card redistributor = PlayCard("ElementalRedistributor");
+
+            SetHitPoints(battalion, 1);
+            SetHitPoints(redistributor, 1);
+
+            //Destroy a target with 1 HP.
+            DecisionSelectCards = new Card[] { redistributor };
+
+            UseIncapacitatedAbility(az, 2);
+
+            AssertInTrash(baron, redistributor);
+            AssertInPlayArea(baron, battalion);
+
+        }
+    }
+}
